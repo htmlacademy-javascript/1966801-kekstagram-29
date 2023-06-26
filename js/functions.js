5a64feb2a817158bd31fc978d551ab553b0dbc73
Taranovskaya <s5198653@yandex.ru>
2023-06-26 10:27:52 +0300

    Добавляет функцию для проверки времени встречи

--- js/functions.js
+++ js/functions.js
@@ -38,3 +38,25 @@ getNumbers('ECMAScript 2022');
 getNumbers('1 кефир, 0.5 батона');
 getNumbers('агент 007');
 getNumbers('а я томат');
+
+// Функция, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи
+// в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+
+const parseTime = (timeString) => {
+  const [hours, minutes] = timeString.split(':');
+  return 60 * hours + Number(minutes);
+};
+
+export const isMeetOnLimit = (workStart, workEnd, meetingStart, meetingLength) => {
+  const workStartInMin = parseTime(workStart);
+  const workEndInMin = parseTime(workEnd);
+  const meetingStartInMin = parseTime(meetingStart);
+
+  return meetingStartInMin >= workStartInMin && meetingStartInMin + meetingLength <= workEndInMin;
+};
+
+isMeetOnLimit('08:00', '17:30', '14:00', 90);
+isMeetOnLimit('8:0', '10:0', '8:0', 120);
+isMeetOnLimit('08:00', '14:30', '14:00', 90);
+isMeetOnLimit('14:00', '17:30', '08:0', 90);
+isMeetOnLimit('8:00', '17:30', '08:00', 900);
